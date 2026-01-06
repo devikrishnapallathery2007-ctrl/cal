dfb3112b1757f067a89d8a3c7e9d129d100dd1ea
devikrishnapallathery2007-ctrl <devikrishnapallathery2007@gmail.com>
2026-01-06 12:24:47 +0530

    Initial commit of calculator application

---
+++ script.js
@@ -0,0 +1,30 @@
+const resultInput = document.getElementById('result');
+
+function appendNumber(number) {
+    resultInput.value += number;
+}
+
+function appendOperator(operator) {
+    resultInput.value += operator;
+}
+
+function clearResult() {
+    resultInput.value = '';
+}
+
+function deleteLast() {
+    resultInput.value = resultInput.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        const result = eval(resultInput.value);
+        if (Number.isFinite(result)) {
+            resultInput.value = result;
+        } else {
+            resultInput.value = 'Error';
+        }
+    } catch (error) {
+        resultInput.value = 'Error';
+    }
+}
